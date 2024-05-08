41dde7c6786bbab0aeba1e1ddc1ea7c0922cf444
Krasimir Kostadinov <krasimir.kostadinov.dev@gmail.com>
2024-05-08 22:52:32 +0300

    added Loader

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -14,24 +14,71 @@ async function getSalesData() {
     _count: true,
   })
 
+  await wait(5000)
+
   return {
     amount: (salesData._sum.pricePaidInCents || 0) / 100,
     numSales: salesData._count,
   }
 }
 
+function wait(duration: number) {
+  return new Promise((resolve) => setTimeout(resolve, duration))
+}
+
+async function getCustomersData() {
+  const [usersCount, orderData] = await Promise.all([
+    prisma.user.count(),
+    prisma.order.aggregate({
+      _sum: { pricePaidInCents: true },
+    }),
+  ])
+
+  return {
+    usersCount,
+    averageValuePerUser:
+      usersCount === 0
+        ? 0
+        : (orderData._sum.pricePaidInCents || 0) / usersCount,
+  }
+}
+
+async function getProductData() {
+  const [activeCount, inactiveCount] = await Promise.all([
+    prisma.product.count({ where: { isAvailableForPurchase: true } }),
+    prisma.product.count({ where: { isAvailableForPurchase: false } }),
+  ])
+
+  return {
+    activeCount,
+    inactiveCount,
+  }
+}
+
 export default async function AdminDashboard() {
   const salesData = await getSalesData()
+  const customersData = await getCustomersData()
+  const productData = await getProductData()
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
       <DashBoardCard
         title='Sales'
-        subtitle={formatNumber(salesData.numSales) + ' orders'}
+        subtitle={formatNumber(salesData.numSales) + ' Orders'}
         body={formatCurrency(salesData.amount)}
       />
-      {/* <DashBoardCard title="Sales" subtitle="Test" body="body" /> */}
-      {/* <DashBoardCard title="Sales" subtitle="Test" body="body" /> */}
+      <DashBoardCard
+        title='Customers'
+        subtitle={
+          formatCurrency(customersData.averageValuePerUser) + ' Average Value'
+        }
+        body={formatNumber(customersData.usersCount) + ' users'}
+      />
+      <DashBoardCard
+        title='Active Products'
+        subtitle={formatNumber(productData.inactiveCount) + ' Inactive'}
+        body={formatNumber(productData.activeCount)}
+      />
       {/* <DashBoardCard title="Sales" subtitle="Test" body="body" /> */}
     </div>
   )
